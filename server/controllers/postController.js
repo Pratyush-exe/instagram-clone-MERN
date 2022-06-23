c3e5949e9e5982d2eb1002362bfb9a6afe2ce971
Pratyush-exe <pratyush21225@gmail.com>
2022-06-23 09:20:40 +0530

    added createPost

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -10,3 +10,18 @@ export const getPosts = async (req, res) => {
         res.status(404).json({message: error.message})
     }
 }
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostModel({...post, 
+        creator: req.userId, 
+        createdAt: new Date().toISOString()
+    })
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
+}
