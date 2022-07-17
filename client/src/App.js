c47f3c01f97e68a7371e0a06de6be5d65b66cfde
Pratyush-exe <pratyush21225@gmail.com>
2022-07-17 08:23:20 +0530

    added signup/signin

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Home from "./Components/Home/Home";
 import NavBar from "./Components/NavBar/NavBar";
 import Post from "./Components/Posts/Post/Post";
 import PostsMain from "./Components/Posts/PostsMain";
+import SignUp from './Components/SignUp/SignUp'
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <PostsMain />
+      {/* <NavBar />
+      <PostsMain /> */}
+      <Home />
     </div>
   );
 }
---
+++ client/src/Components/SignUp/SignUp.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './SignUp.css'
+
+function SignUp() {
+  return (
+    <div className='creds-form-signup-cont'>
+        <div className='creds-form-signup'>
+            <img style={{paddingBottom: '20px'}} src='https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png' />
+            <div style={{display: 'flex', flexDirection: 'column'}}>
+                <input style={{ width: '300px', height: '30px', marginBottom: '5px', paddingLeft: '5px'}} type={"text"} placeholder='Username' />
+                <button style={{ width: '310px', height: '30px', marginBottom: '5px'}}>Login Google</button>
+            </div>
+        </div>
+        <div style={{display: 'flex', flexDirection: 'row', border: '1px solid lightgray', justifyContent: 'center'}}>
+            <p>Have an account?</p><p className='login-bt'>Log in</p>
+        </div>
+    </div>
+  )
+}
+
+export default SignUp
